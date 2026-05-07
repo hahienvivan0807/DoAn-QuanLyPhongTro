64ab9dd16300300f8a12667fb26e6b732cbfc615
Yến Ngọc <ngoc07966@gmail.com>
2026-05-07 18:54:38 +0700

    Add project files.

---
+++ QuanLyNhaTro/Pages/ChiTietPhong.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuanLyNhaTro.Pages
+{
+    public class ChiTietPhongModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ QuanLyNhaTro/Pages/Manager.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuanLyNhaTro.Pages
+{
+    public class ManagerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
