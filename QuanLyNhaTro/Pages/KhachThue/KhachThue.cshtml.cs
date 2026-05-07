e1448e9fd303207fb822deda33af3591873a9c21
pinvutru <pinvutru@DESKTOP-HBBLO71>
2026-05-07 21:06:35 +0700

    Thêm khách thuê

---
+++ QuanLyNhaTro/Pages/KhachThue/KhachThue.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuanLyNhaTro.Pages.KhachThue
+{
+    public class KhachThueModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
