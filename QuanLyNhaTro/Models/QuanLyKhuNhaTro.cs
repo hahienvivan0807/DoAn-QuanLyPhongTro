b577dc623e27c33e7f34380c9ca197b6e33203bb
pinvutru <pinvutru@DESKTOP-HBBLO71>
2026-05-08 11:05:44 +0700

    thêm Controllers và Models

--- QuanLyNhaTro/Models/QuanLyKhuNhaTro.cs
+++ QuanLyNhaTro/Models/QuanLyKhuNhaTro.cs
@@ -6,6 +6,14 @@ namespace QuanLyNhaTro.Models
 {
     public class ACCOUNT
     {
-        [key]
+        [Key] public string IDUser { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Roles { get; set; }
+        public string QR_Link { get; set; }
+        public DateTime CreatedAt { get; set; }
+
     }
 }
